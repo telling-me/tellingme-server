7c4aee93e933245848272de12bbc08a80c363e7d
kitaee <dlrlxo999@naver.com>
2023-02-24 17:22:45 +0900

    🔨 [MOD] : deploy.sh 파일 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/test
-PROJECT_NAME=server
+REPOSITORY=/home/ubuntu/app/build/libs
 
-echo ">>> Build 파일 복사"
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-cp $REPOSITORY/deploy/*.jar $REPOSITORY/
+CURRENT_PID=$(lsof -ti tcp:8080)
 
-echo ">>> 현재 구동중인 애플리케이션 pid 확인"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-CURRENT_PID=$(pgrep -fl jenkins-springboot | grep java | awk '{print $1}')
-
-echo ">> PID : " $CURRENT_PID
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "구동중인 애플리케이션 없음."
+if [ -z "${CURRENT_PID}" ]
+then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo ">>> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 10
+  echo "> kill -9 $CURRENT_PID"
+  sudo kill -9 "$CURRENT_PID"
+  sleep 5
 fi
 
-echo ">>> 애플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
-echo ">>> JAR NAME : $JAR_NAME"
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo ">>> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=/home/ubuntu/test/application-mysql.properties -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=/home/ubuntu/app/src/main/resources/application-mysql.properties,/home/ubuntu/app/src/main/resources/application-s3.properties,/home/ubuntu/app/src/main/resources/application-security.properties -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+
+echo "> 기태 짱"
